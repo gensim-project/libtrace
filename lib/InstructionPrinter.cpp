41ba18ad61ff97237c8d33ea4d68284e0ce2440a
Harry Wagstaff <fred.the.fish@gmail.com>
2017-10-12 15:19:02 +0100

    Initial support for 64-bit values in most records

--- lib/InstructionPrinter.cpp
+++ lib/InstructionPrinter.cpp
@@ -5,8 +5,29 @@
 #include <cassert>
 #include <iomanip>
 #include <sstream>
+#include <vector>
 
 using namespace libtrace;
+
+class DataPrinter {
+public:
+	DataPrinter(uint32_t data_low, const InstructionPrinter::extension_list_t &extensions) : data_low_(data_low), extensions_(extensions) {
+		
+	}
+	
+	friend std::ostream& operator<< (std::ostream& stream, const DataPrinter& matrix) {
+		stream << "0x" << std::hex << std::setw(8) << std::setfill('0');
+		for(auto i : matrix.extensions_) {
+			stream << i.GetData();
+		}
+		stream << matrix.data_low_;
+		return stream;
+	}
+	
+private:
+	uint32_t data_low_;
+	const InstructionPrinter::extension_list_t &extensions_;
+};
 
 InstructionPrinter::InstructionPrinter()
 {
@@ -34,23 +55,32 @@ bool InstructionPrinter::PrintInstruction(std::ostream &str, RecordIterator &it,
 	
 	str << "[" << std::hex << std::setw(8) << std::setfill('0') << hdr->GetPC() << "] " << std::hex << std::setw(8) << std::setfill('0') << code->GetIR() << " ";
 	
+	std::vector<DataExtensionRecord> extensions;
 	while(it != end && (*it).GetType() != InstructionHeader) {
 		// do a thing
 		TraceRecord rcd = *it;
 		TraceRecord *record = &rcd;
 		
+		if(record->GetType() == DataExtension) {
+			extensions.push_back(*(DataExtensionRecord*)&rcd);
+			continue;
+		}
+		
+		assert(extensions.size() == record->GetExtensionCount());
+		
 		switch((*it).GetType()) {
-			case RegRead: PrintRegRead(str, (RegReadRecord*)record); break;
-			case RegWrite: PrintRegWrite(str, (RegWriteRecord*)record); break;
-			case BankRegRead: PrintBankRegRead(str, (BankRegReadRecord*)record); break;
-			case BankRegWrite: PrintBankRegWrite(str, (BankRegWriteRecord*)record); break;
-			case MemReadAddr: PrintMemRead(str, it); break;
-			case MemWriteAddr: PrintMemWrite(str, it); break;
-			
+			case RegRead: PrintRegRead(str, (RegReadRecord*)record, extensions); break;
+			case RegWrite: PrintRegWrite(str, (RegWriteRecord*)record, extensions); break;
+			case BankRegRead: PrintBankRegRead(str, (BankRegReadRecord*)record, extensions); break;
+			case BankRegWrite: PrintBankRegWrite(str, (BankRegWriteRecord*)record, extensions); break;
+			case MemReadAddr: PrintMemRead(str, it, extensions); break;
+			case MemWriteAddr: PrintMemWrite(str, it, extensions); break;
 			default:
 				fprintf(stderr, "Unsupported record type %u at %lu\n", (*it).GetType(), it.index());
 				assert(false && "Unsupported record type");
 		}
+		
+		extensions.clear();
 		
 		it++;
 	}
@@ -58,7 +88,7 @@ bool InstructionPrinter::PrintInstruction(std::ostream &str, RecordIterator &it,
 	return true;
 }
 
-bool InstructionPrinter::PrintRegRead(std::ostream &str, RegReadRecord* rec)
+bool InstructionPrinter::PrintRegRead(std::ostream &str, RegReadRecord* rec, const extension_list_t& extensions)
 {
 	assert(rec->GetType() == RegRead);
 	if(!_print_reg_read) return true;
@@ -67,7 +97,7 @@ bool InstructionPrinter::PrintRegRead(std::ostream &str, RegReadRecord* rec)
 	return true;
 }
 
-bool InstructionPrinter::PrintRegWrite(std::ostream &str, RegWriteRecord* rec)
+bool InstructionPrinter::PrintRegWrite(std::ostream &str, RegWriteRecord* rec, const extension_list_t& extensions)
 {
 	assert(rec->GetType() == RegWrite);
 	if(!_print_reg_write) return true;
@@ -76,21 +106,27 @@ bool InstructionPrinter::PrintRegWrite(std::ostream &str, RegWriteRecord* rec)
 	return true;
 }
 
-bool InstructionPrinter::PrintBankRegRead(std::ostream &str, BankRegReadRecord *rec){
+bool InstructionPrinter::PrintBankRegRead(std::ostream &str, BankRegReadRecord *rec, const extension_list_t& extensions){
 	if(!_print_bank_read) return true;
 	str << "(R[" << (uint32_t)rec->GetBank() << "][" << (uint32_t)rec->GetRegNum() <<"] -> 0x" << std::hex << std::setw(8) << std::setfill('0') << rec->GetData() << ")";
 	return true;
 }
 
-bool InstructionPrinter::PrintBankRegWrite(std::ostream &str, BankRegWriteRecord *rec){
+bool InstructionPrinter::PrintBankRegWrite(std::ostream &str, BankRegWriteRecord *rec, const extension_list_t& extensions){
 	if(!_print_bank_write) return true;
 	str << "(R[" << (uint32_t)rec->GetBank() << "][" << (uint32_t)rec->GetRegNum() <<"] <- 0x" << std::hex << std::setw(8) << std::setfill('0') << rec->GetData() << ")";
 	return true;
 }
 
-bool InstructionPrinter::PrintMemRead(std::ostream &str, RecordIterator &it){
+bool InstructionPrinter::PrintMemRead(std::ostream &str, RecordIterator &it, const extension_list_t& addr_extensions){
 	
 	TraceRecord raddr = *it++;
+	extension_list_t data_extensions;
+	while((*it).GetType() == DataExtension) {
+		TraceRecord tr = *it;
+		data_extensions.push_back(*(DataExtensionRecord*)&tr);
+		it++;
+	}
 	TraceRecord rdata = *it;
 	
 	if(!_print_mem_read) return true;
@@ -98,12 +134,19 @@ bool InstructionPrinter::PrintMemRead(std::ostream &str, RecordIterator &it){
 	MemReadAddrRecord *addr = (MemReadAddrRecord*)&raddr;
 	MemReadDataRecord *data = (MemReadDataRecord*)&rdata;
 	
-	str << "([" << std::hex << std::setw(8) << std::setfill('0') << addr->GetAddress() << "](" << (uint32_t)addr->GetWidth() << ") => 0x" << std::hex << std::setw(8) << std::setfill('0') << data->GetData() << ")";
+	str << "([" << DataPrinter(addr->GetAddress(), addr_extensions) << "](" << (uint32_t)addr->GetWidth() << ") => " << DataPrinter(data->GetData(), data_extensions) << ")";
 	
 	return true;
 }
-bool InstructionPrinter::PrintMemWrite(std::ostream &str, RecordIterator &it){	
+bool InstructionPrinter::PrintMemWrite(std::ostream &str, RecordIterator &it, const extension_list_t& addr_extensions){	
 	TraceRecord raddr = *it++;
+	
+	extension_list_t data_extensions;
+	while((*it).GetType() == DataExtension) {
+		TraceRecord tr = *it;
+		data_extensions.push_back(*(DataExtensionRecord*)&tr);
+		it++;
+	}
 	TraceRecord rdata = *it;
 	
 	if(!_print_mem_write) return true;
@@ -111,7 +154,7 @@ bool InstructionPrinter::PrintMemWrite(std::ostream &str, RecordIterator &it){
 	MemWriteAddrRecord *addr = (MemWriteAddrRecord*)&raddr;
 	MemWriteDataRecord *data = (MemWriteDataRecord*)&rdata;
 	
-	str << "([" << std::hex << std::setw(8) << std::setfill('0') << addr->GetAddress() << "](" << (uint32_t)addr->GetWidth() << ") <= 0x" << std::hex << std::setw(8) << std::setfill('0') << data->GetData() << ")";
-	
+	str << "([" << DataPrinter(addr->GetAddress(), addr_extensions) << "](" << (uint32_t)addr->GetWidth() << ") <= " << DataPrinter(data->GetData(), data_extensions) << ")";
 	return true;
 }
+
